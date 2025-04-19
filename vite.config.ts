5e1e1189a6dd23003d1d15986f5d7c61ed85f30f
peterdinis <petter1@pobox.sk>
2025-04-19 10:49:54 +0200

    infra: Update prettier

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [
         TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
