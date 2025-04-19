2c26a53223d407cde6f8f0c00e413fa5ae7d15c6
peterdinis <petter1@pobox.sk>
2025-04-19 10:18:45 +0200

    infra: Adding tanstack router WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import { Toaster } from 'sonner';
-import Header from './components/home/Header';
-import FileContent from './components/files/FileContent';
-import SharedFile from './components/files/SharedFile';
+import { Outlet, RootRoute, Route, Router, RouterProvider } from "@tanstack/react-router";
+import Header from "./components/home/Header";
+import Hero from "./components/home/Hero";
+import { SignInForm } from "./components/auth/SignInForm";
+import ShareFile from "./components/files/SharedFile";
 
-export default function App() {
-    const shareCode = new URLSearchParams(window.location.search).get('share');
-
-    return (
-        <div className='flex min-h-screen flex-col'>
+const rootRoute = new RootRoute({
+    component: () => (
+        <div className="flex min-h-screen flex-col">
             <Header />
-            <main className='flex-1 p-8'>
-                <div className='mx-auto max-w-4xl'>
-                    {shareCode ? (
-                        <SharedFile shareCode={shareCode} />
-                    ) : (
-                        <FileContent />
-                    )}
+            <main className="flex-1 p-8">
+                <div className="mx-auto max-w-4xl">
+                    <Outlet />
                 </div>
             </main>
-            <Toaster />
         </div>
+    ),
+});
+
+const indexRoute = new Route({
+    getParentRoute: () => rootRoute,
+    path: '/',
+    component: Hero
+});
+
+const loginRoute = new Route({
+    getParentRoute: () => rootRoute,
+    path: '/login',
+    component: SignInForm
+})
+
+const shareRoute = new Route({
+    getParentRoute: () => rootRoute,
+    path: '/share',
+    component: ShareFile
+})
+
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router
+    }
+}
+
+const routeTree = rootRoute.addChildren([indexRoute, loginRoute, shareRoute]);
+
+const router = new Router({ routeTree });
+
+export default function App() {
+    return (
+        <>
+            <RouterProvider router={router} />
+        </>
     );
 }
